e8c0dd6ab9f2833090837ecfdeb87a33394c1645
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2024-09-24 14:03:12 +0900

    Update model.py

--- cgcnn/model.py
+++ cgcnn/model.py
@@ -205,7 +205,7 @@ class CrystalGraphConvNet(nn.Module):
         
         # if self.classification:
             # out = self.logsoftmax(out)
-        return tuple(head(crys_fea) for head in self.heads))
+        return tuple(head(crys_fea) for head in self.heads)
 
     def pooling(self, atom_fea, crystal_atom_idx):
         """
