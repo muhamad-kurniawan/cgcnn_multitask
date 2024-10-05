3b9fb6f999836dc24dd0e29330663b1938500f49
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2024-10-05 19:24:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -529,7 +529,7 @@ def validate(val_loader, model, criterions, normalizers, tasks, epoch, test=Fals
       batch_time.update(time.time() - end)
       end = time.time()
       embedding = embedding.cpu().numpy().tolist()
-      if epoch%25==0:
+      if epoch%25==0 or test=True:
         for idx_, cid in enumerate(cif_id):
           embedding_dict[cid] = embedding [idx_]
         if test== False:
