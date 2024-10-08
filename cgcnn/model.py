f13ad4ab384e5e3a3c29f121496b88dac4df365a
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2024-10-08 13:56:16 +0900

    Update model.py

--- cgcnn/model.py
+++ cgcnn/model.py
@@ -456,3 +456,62 @@ class OutNetwork(nn.Module):
         output_dims = [fc_out.out_features for fc_out in self.fc_outs]
         activation = type(self.acts[0]).__name__
         return f"{type(self).__name__}({input_dim=}, {output_dims=}, {activation=})"    
+
+class ResidualNetwork(nn.Module):
+    """Feed forward Residual Neural Network."""
+
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        hidden_layer_dims: Sequence[int],
+        activation: type[nn.Module] = nn.ReLU,
+        batch_norm: bool = False,
+    ) -> None:
+        """Create a feed forward neural network with skip connections.
+
+        Args:
+            input_dim (int): Number of input features
+            output_dim (int): Number of output features
+            hidden_layer_dims (list[int]): List of hidden layer sizes
+            activation (type[nn.Module], optional): Which activation function to use.
+                Defaults to nn.LeakyReLU.
+            batch_norm (bool, optional): Whether to use batch_norm. Defaults to False.
+        """
+        super().__init__()
+
+        dims = [input_dim, *list(hidden_layer_dims)]
+
+        self.fcs = nn.ModuleList(
+            nn.Linear(dims[idx], dims[idx + 1]) for idx in range(len(dims) - 1)
+        )
+
+        if batch_norm:
+            self.bns = nn.ModuleList(
+                nn.BatchNorm1d(dims[idx + 1]) for idx in range(len(dims) - 1)
+            )
+        else:
+            self.bns = nn.ModuleList(nn.Identity() for _ in range(len(dims) - 1))
+
+        self.res_fcs = nn.ModuleList(
+            nn.Linear(dims[idx], dims[idx + 1], bias=False)
+            if (dims[idx] != dims[idx + 1])
+            else nn.Identity()
+            for idx in range(len(dims) - 1)
+        )
+        self.acts = nn.ModuleList(activation() for _ in range(len(dims) - 1))
+
+        self.fc_out = nn.Linear(dims[-1], output_dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Forward pass through network."""
+        for fc, bn, res_fc, act in zip(self.fcs, self.bns, self.res_fcs, self.acts):
+            x = act(bn(fc(x))) + res_fc(x)
+
+        return self.fc_out(x)
+
+    def __repr__(self) -> str:
+        input_dim = self.fcs[0].in_features
+        output_dim = self.fc_out.out_features
+        activation = type(self.acts[0]).__name__
+        return f"{type(self).__name__}({input_dim=}, {output_dim=}, {activation=})"
